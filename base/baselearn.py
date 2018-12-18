6b05c6f26f53453991ceb41e985aa4ee8e6d263e
meiziyuan <mmdd5714@163.com>
2018-12-18 15:20:09 +0800

    学习数据类型

---
+++ base/baselearn.py
@@ -0,0 +1,20 @@
+
+##数据类型
+#转义字符\  也可以在字符串前面加上r
+print("I\'m ok")
+print(r"I'm ok")
+
+##IDE一行输入不完换行输入 直接在最后enter键即可
+print("sss"
+      "aaa"
+      "bbb")
+
+##输出文本中多行换行,采用格式'''   '''   在命令行中，>>>代替...。...是提示符的,不是代码一部分
+print('''sss
+aaa
+bbb''')
+
+print(r'''I'm ok \n hello''')
+
+##布尔值可以用and or not
+print(True or False)
