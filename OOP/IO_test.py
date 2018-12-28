a19bb1b1d7e03ccbc4c750531f0e8e706d91999e
meiziyuan <mmdd5714@163.com>
2018-12-28 14:31:07 +0800

    add IO

--- OOP/IO_test.py
+++ OOP/IO_test.py
@@ -10,7 +10,7 @@
 import logging
 
 try:
-    f = open("../testfiles/test",'r') ##以读文件的模式打开一个文件对象，使用内置的open()函数
+    f = open("../testfiles/test",'rb') ##以读文件的模式打开一个文件对象，使用内置的open()函数
     print(f.read())  ##调用read()方法可以一次性读取文件的全部内容，python把文件读取到内存中，是用一个str表示。
 except IOError as e:
     logging.info(e)
@@ -25,12 +25,69 @@ with open("../testfiles/test",'r') as f1:
     print(f1.readlines())
 
 with open("../testfiles/test",'w') as ww:
-    ww.write("我在写入")
+    ww.write("test2")
 
 with open("../testfiles/test",'a') as ww:
-    ww.write("我在末尾追加")
+    ww.write("test1")
 
 #调用read()会一次性读取文件的全部内容，如果文件有10G，内存就爆了，
 # 所以，要保险起见，可以反复调用read(size)方法，每次最多读取size个字节的内容。
 # 另外，调用readline()可以每次读取一行内容，调用readlines()一次读取所有内容并按行返回list。
 
+
+##StringIO:在内存中读写str，要把str写入StringIO，需要先创建一个StringIO，然后像文件一样写入
+from io import StringIO
+f = StringIO()
+f.write("hello")
+print(f.getvalue())  #getvalue()方法用于写入后的str
+
+f1 = StringIO('你好')
+print(f1.read())
+
+#StringIO操作的是str,如果要操作二进制数据，就需要使用BytesIO。
+from io import BytesIO
+bb = BytesIO()
+bb.write("中文".encode('utf-8'))   #写入的不是str，而是经过UTF-8编码的bytes。
+print(bb.getvalue())
+print(bb.getvalue().decode('utf-8'))
+
+#或者如下：
+bw = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+print(bw.read().decode('utf-8'))
+
+
+##文件目录操作
+import os
+print(os.name)   ##如果是posix，说明系统是Linux、Unix或Mac OS X，如果是nt，就是Windows系统。
+#print(os.uname())  ##在window上不提供
+
+##在操作系统中定义的环境变量，全部保存在os.environ这个变量中，可以直接查看
+print(os.environ)
+
+#要获取某个环境变量的值，可以调用os.environ.get('key')
+print(os.environ.get('ANDROID_HOME'))
+print(os.environ.get('PATH'))
+
+#操作文件和目录的函数一部分放在os模块中，一部分放在os.path模块中
+#查看当前目录的绝对路径
+print(os.path.abspath("."))
+
+
+
+##我们把变量从内存中变成可存储或传输的过程称之为序列化，在Python中叫pickling
+#序列化之后，就可以把序列化后的内容写入磁盘，或者通过网络传输到别的机器上。
+#反过来，把变量内容从序列化的对象重新读到内存里称之为反序列化，即unpickling。
+import pickle
+d = dict(name= 'yuan',age = 20)
+pickle.dumps(d)
+f = open("../testfiles/test",'wb')
+pickle.dump(d,f)
+f.close()
+
+import json
+json_str = '{"name":"yuan" , "age":25 }'
+print(json.loads(json_str))  ##将str转换为dict类型
+print(type(json.loads(json_str)))
+
+print(json.dumps(d))
+print(type(json.dumps(d))) ##将dict转换为一个str类型
